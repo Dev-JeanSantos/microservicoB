7bf3a906b5816a12f17962b5b6ec5e3f567e79fd
Dev-JeanSantos <jeancbsan@gmail.com>
2022-11-09 21:48:42 -0300

    Criação microsserviçoB

---
+++ src/main/kotlin/com/example/microservicoB/MicroservicoBApplication.kt
@@ -0,0 +1,11 @@
+package com.example.microservicoB
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicroservicoBApplication
+
+fun main(args: Array<String>) {
+	runApplication<MicroservicoBApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/microservicoB/MicroservicoBApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.microservicoB
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroservicoBApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
