7149b305e0cffa66a3667ad288ab9e79ae63d8f1
Dev-JeanSantos <jeancbsan@gmail.com>
2022-11-10 06:57:50 -0300

    criando recursos s3 com permissoes de publica de leitura

--- ci/localstack/create-resources.sh
+++ ci/localstack/create-resources.sh
@@ -1,4 +1,11 @@
 export DEFAULT_REGION=us-east-1
 export AWS_DEFAULT_REGION=us-east-1
 #!/bin/bash
-echo "[INFO] - DESAFIO - SETUP LOCALSTACK"
+echo "[INFO] - MICROSERVIÇO-B - LOCALSTACK"
+
+echo "[INFO] - CRIAÇÃO S3"
+aws --endpoint-url http://localhost:4566 --region us-east-1  s3api create-bucket --bucket mybucket
+
+echo "[INFO] - PERMISSÕES S3"
+aws --endpoint-url http://localhost:4566 s3api put-bucket-acl --bucket mybucket --acl public-read
+
